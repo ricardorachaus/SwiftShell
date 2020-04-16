1997eeb7504897a4462bf0be2b4a4eff52b7f4ed
Ricardo Rachaus <ricardorachaus@gmail.com>
2020-04-16 17:19:20 -0300

    Removing Test Target

--- Package.swift
+++ Package.swift
@@ -6,23 +6,15 @@ import PackageDescription
 let package = Package(
     name: "SwiftShell",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftShell",
-            targets: ["SwiftShell"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: ["SwiftShell"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftShell",
-            dependencies: []),
-        .testTarget(
-            name: "SwiftShellTests",
-            dependencies: ["SwiftShell"]),
+            dependencies: []
+        )
     ]
 )
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import SwiftShellTests
-
-var tests = [XCTestCaseEntry]()
-tests += SwiftShellTests.allTests()
-XCTMain(tests)
